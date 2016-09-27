e2fe6df2c2cc7039aec9452aeb547891fa89cf59
Paweł Marut <pma@crm.commit.pl>
2016-09-27 21:29:51 +0200

    Initial commit

---
+++ src/main/java/marp/view/Application.java
@@ -0,0 +1,14 @@
+package marp.view;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by pma on 2016-09-19.
+ */
+@SpringBootApplication
+public class Application {
+    public static  void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
