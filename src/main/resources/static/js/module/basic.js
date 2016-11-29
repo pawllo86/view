e8f71863c1cdeb75fdebd12ae8daca2ee6aca125
Paweł Marut <pma@crm.commit.pl>
2016-11-29 22:44:20 +0100

    Add angular js

---
+++ src/main/resources/static/js/module/basic.js
@@ -0,0 +1,9 @@
+/**
+ * Created by pma on 2016-11-27.
+ */
+angular.module('basicModule', []).component('marpPreloader', {
+    templateUrl: 'templates/preloader.html',
+    controller: function () {
+        
+    }
+});
---
+++ src/main/resources/static/js/template/preloader.js
@@ -0,0 +1,9 @@
+/**
+ * Created by pma on 2016-11-27.
+ */
+
+$(window).on('load', function() {
+    'use strict';
+
+    $("#preloader").delay(1500).fadeOut(500);
+});
