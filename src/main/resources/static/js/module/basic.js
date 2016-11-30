1f2dbf481658173af48e3602e350083d59019d76
Paweł Marut <pmarut@interia.pl>
2016-11-30 23:59:50 +0100

    Create directives for preloader and carousel

--- src/main/resources/static/js/module/basic.js
+++ src/main/resources/static/js/module/basic.js
@@ -1,9 +1,51 @@
 /**
  * Created by pma on 2016-11-27.
  */
-angular.module('basicModule', []).component('marpPreloader', {
-    templateUrl: 'templates/preloader.html',
-    controller: function () {
-        
-    }
-});
+angular.module('basicModule', [])
+    .directive('marpPreloader', function() {
+        return {
+            templateUrl: 'templates/preloader.html',
+
+            link: function() {
+                $(window).load(function () {
+                    $(".preloader").delay(1500).fadeOut(100);
+                });
+            }
+        }
+    })
+    .directive('marpCarousel', ['$window', function ($window) {
+        return {
+            templateUrl: 'templates/carousel.html',
+            controller: function ($scope) {
+                $scope.sliders = [{
+                    image: '../images/slider/slide0.jpg',
+                    title: 'Sonoterapia',
+                    description: 'Terapia ultradźwiękowa w miejscu bólu',
+                    active: true
+                },{
+                    image: '../images/slider/slide1.jpg',
+                    title: 'Magnetoterapia',
+                    description: 'Terapia elektomagnesem w miejscu bólu',
+                    active: false
+                },{
+                    image: '../images/slider/slide2.jpg',
+                    title: 'Elektroterapia',
+                    description: 'Terapia prądem w miejscu bólu',
+                    active: false
+                },{
+                    image: '../images/slider/slide3.jpg',
+                    title: 'Laseroterapia',
+                    description: 'Terapia laserem w miejscu bólu',
+                    active: false
+                }];
+            },
+            link: function() {
+                angular.element($window).load(function () {
+                    $('#home .carousel-inner .item').css('height', $window.innerHeight);
+                });
+                angular.element($window).bind('resize', function () {
+                    $('#home .carousel-inner .item').css('height', $window.innerHeight);
+                });
+            }
+        }
+    }]);
--- src/main/resources/static/js/template/preloader.js
+++
@@ -1,9 +0,0 @@
-/**
- * Created by pma on 2016-11-27.
- */
-
-$(window).on('load', function() {
-    'use strict';
-
-    $("#preloader").delay(1500).fadeOut(500);
-});
