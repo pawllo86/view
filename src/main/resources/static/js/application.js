b8131f95553ef1263811bd2925c5336e81f92097
Paweł Marut <pmarut@interia.pl>
2016-12-08 20:01:03 +0100

    Add menu

--- src/main/resources/static/js/application.js
+++ src/main/resources/static/js/application.js
@@ -1,4 +1,12 @@
 /**
  * Created by pma on 2016-11-27.
  */
-angular.module('marpApp', ['basicModule']);
+angular.module('marpApp', ['basicModule']).run(function() {
+    angular.element(document).ready(function () {
+        $('.navbar-collapse ul li a').click(function() {
+            $('html, body').animate({scrollTop: $(this.hash).offset().top - 79}, 1000);
+
+            return false;
+        });
+    });
+});
